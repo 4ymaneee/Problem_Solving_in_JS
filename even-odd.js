28ccb9c9add8c45481536589bd5bc426429994c9
4ymaneee <nalmlwmyat4@gmail.com>
2023-10-18 16:02:37 +0100

    first problem solving in Js

---
+++ even-odd.js
@@ -0,0 +1,18 @@
+// methode 1
+function even_or_odd(number){
+    if(number % 2 === 0){
+        return "Even"
+    } else {
+        return "Odd"
+    }
+}
+
+console.log(even_or_odd(11))
+
+// methode 2 
+
+function even_or_oddd(numbeer){
+    return numbeer % 2 === 0 ? "Even":"Odd"
+}
+
+console.log(even_or_oddd(1002))
