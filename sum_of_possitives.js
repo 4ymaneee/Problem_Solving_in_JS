149eb2032594b6ee24d1ec5e48d1cac2718d8641
4ymaneee <nalmlwmyat4@gmail.com>
2023-10-18 16:36:52 +0100

    some modification

--- sum_of_possitives.js
+++ sum_of_possitives.js
@@ -1,11 +1,11 @@
-function positiveSum(arr){
-    let sum = 0
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] > 0){
-            sum += arr[i]
-        }
+function positiveSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
     }
-    return sum
+  }
+  return sum;
 }
 
-console.log(positiveSum([1,2,3,4,5,-20,1,-2,-100]))
+console.log(positiveSum([1, 2, 3, 4, 5, -20, 1, -2, -100]));
