b2e67c00bdb4005ba3b5cb0ecd4c319f48974536
4ymaneee <nalmlwmyat4@gmail.com>
2023-10-19 13:32:39 +0100

    Basic Mathematical Operations

---
+++ Basic Mathematical Operations.js
@@ -0,0 +1,14 @@
+function basicOp(operation, value1, value2)
+{
+  if(operation == '+'){
+    return value1 + value2
+  } else if(operation == '-'){
+    return value1 - value2
+  } else if(operation == '*'){
+    return value1 * value2
+  } else{
+    return value1 / value2
+  }
+}
+
+console.log(basicOp('*',3,4))
