bb7e59d56d3b3869229b58baf2a859cec93d4475
4ymaneee <nalmlwmyat4@gmail.com>
2023-10-19 17:19:37 +0100

    Welcome!

---
+++ Welcome.js
@@ -0,0 +1,29 @@
+//create an object that containe languages
+//loop for all this languages
+//if it the same as the user enter
+//print it
+let data = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+  };
+
+function greet(language) {
+  return data[language] ? data[language]:"Welcome"
+  }
+
+console.log(greet("polish"));
