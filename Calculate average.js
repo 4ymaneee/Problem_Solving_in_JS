9957da9d01022e90e96a46eef1b461355fb34c5c
4ymaneee <nalmlwmyat4@gmail.com>
2023-10-19 16:01:23 +0100

    Calculate average

---
+++ Calculate average.js
@@ -0,0 +1,12 @@
+function findAverage(array) {
+    let sum = 0
+    if(array.length == 0){
+        return 0
+    }
+    for(let i = 0; i < array.length; i++){
+        sum += array[i]
+    }
+    return average = sum / array.length
+  }
+
+console.log(findAverage([]) )
