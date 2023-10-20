a90faeaf7b5ebd45d3c9ad03bcaa7d9f172e2667
4ymaneee <nalmlwmyat4@gmail.com>
2023-10-20 18:01:58 +0100

    No zeros for heros

--- No Zeros For Heros.js
+++ No Zeros For Heros.js
@@ -1,10 +1,10 @@
 function noBoringZeros(n) {
     let string = n.toString();
 
-    while(string.lastIndexOf(0) === string.length - 1){
+    while(string.endsWith(0)){
         string = string.slice(0, string.length - 1)
     }
     return +string
   }
 
-console.log(noBoringZeros(105000))
+console.log(noBoringZeros(10))
