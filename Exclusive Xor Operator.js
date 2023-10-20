2595dbfab2e6055fef8d9a9b4189f53d33314ed5
4ymaneee <nalmlwmyat4@gmail.com>
2023-10-20 02:41:32 +0100

    Exclusive "or" (xor) Logical Operator

---
+++ Exclusive Xor Operator.js
@@ -0,0 +1,11 @@
+function xor(a, b) {
+    if(a == true && b ==true){
+        return false
+    }else if(a == false && b == false){
+        return false
+    }else {
+        return true
+    }
+  }
+
+console.log(xor(true, true))
