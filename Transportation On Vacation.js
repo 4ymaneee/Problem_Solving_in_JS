c9dee29a4e72423ee18669b4d7409233f1e22d0f
4ymaneee <nalmlwmyat4@gmail.com>
2023-10-20 02:32:40 +0100

    Transportation on vacation

---
+++ Transportation On Vacation.js
@@ -0,0 +1,11 @@
+function rentalCarCost(d) {
+  if (d < 3) {
+    return d * 40;
+  } else if (d >= 3 && d < 7) {
+    return d * 40 - 20;
+  } else {
+    return d * 40 - 50;
+  }
+}
+
+console.log(rentalCarCost(6));
