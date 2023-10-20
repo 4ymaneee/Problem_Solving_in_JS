7a760ab3a6cfcd82301e03794517763946076029
4ymaneee <nalmlwmyat4@gmail.com>
2023-10-20 02:46:52 +0100

    Exclusive "or" (xor) Logical Operator

--- Exclusive Xor Operator.js
+++ Exclusive Xor Operator.js
@@ -1,11 +1,5 @@
 function xor(a, b) {
-    if(a == true && b ==true){
-        return false
-    }else if(a == false && b == false){
-        return false
-    }else {
-        return true
-    }
+    return a != b;
   }
 
 console.log(xor(true, true))
