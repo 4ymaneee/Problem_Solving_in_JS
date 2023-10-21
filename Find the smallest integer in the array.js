6803d92e92fb7e73269ba045dcf6d8a31bc95562
4ymaneee <nalmlwmyat4@gmail.com>
2023-10-21 12:26:30 +0100

    Find the smallest integer in the array

---
+++ Find the smallest integer in the array.js
@@ -0,0 +1,11 @@
+function findSmallestInt(args) {
+    let smallest = args[0]
+    for(let i = 0; i < args.length; i++){
+        if(args[i] < smallest){
+            smallest =  args[i]
+        }
+    }
+    return smallest
+}
+
+console.log(findSmallestInt([34, 15, 88, 2,33]));
