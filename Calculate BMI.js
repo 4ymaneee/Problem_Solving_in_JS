dffa9821847633219d3c894edef35b6321f6b657
4ymaneee <nalmlwmyat4@gmail.com>
2023-10-22 12:32:20 +0100

    Calculate BMI

---
+++ Calculate BMI.js
@@ -0,0 +1,6 @@
+function bmi(weight, height) {
+    let bmi = weight / (height * height)
+    return bmi <= 18.5 ? "Underweight": bmi <= 25 ? "Normal": bmi <= 30 ? "Overweight":"Obese"
+  }
+
+console.log(bmi(80, 1.80))
