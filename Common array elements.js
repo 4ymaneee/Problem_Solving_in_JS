cde11d91b6cc743189762a1c7cf62eabdbe4befa
4ymaneee <nalmlwmyat4@gmail.com>
2023-10-23 13:46:15 +0100

    Common array elements

--- Common array elements.js
+++ Common array elements.js
@@ -2,18 +2,13 @@
 // of elements that are common in all three arrays.
 
 
-//create a function
-//create a varibale that sum the commonNumber
-//loop thorugh a
-//check if b includes a[i] && c includes a[i]
-//if true sum the commonNumbers
-//return the final result
-
 function common(a,b,c){
     let commonNumber = 0
-    for(let num of a){
-        if(b.includes(num) && c.includes(num)){
-            commonNumber += num
+    for(let i in a){
+        if(b.indexOf(a[i]) != -1 && c.indexOf(a[i])!= -1){
+            commonNumber += a[i]
+            b.splice(b.indexOf(a[i]),1);
+            c.splice(c.indexOf(a[i]),1);
         }
     }
     return commonNumber
